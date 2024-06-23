64b32fc13f46e5ddda96ce18788048dc8e23cc65
applusinc <help.applusinc@gmail.com>
2024-06-23 17:15:19 +0300

    ping update

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => res.status(200).end('Hello World!'));
-app.get('/', (req, res) => {
+app.get('/ping', (req, res) => {
   res.status(200).end('ping');
 })
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));

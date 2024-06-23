4e59737947dd82eec94936767602e8a580f901ea
applusinc <help.applusinc@gmail.com>
2024-06-23 16:45:56 +0300

    hello world update

---
+++ main.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.status(200).end('Hello World!'));
+app.get('/', (req, res) => {
+  res.status(200).end('ping');
+})
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));

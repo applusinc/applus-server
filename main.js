2cabad90b6bc7c5c9802dffdcb901f3d3e3cf5c3
applusinc <help.applusinc@gmail.com>
2024-07-01 11:01:25 +0300

    fix update2

--- main.js
+++ main.js
@@ -1,20 +1,17 @@
 
 const serverRouter = require("./routes/server/server");
+const httpProxy = require('http-proxy');
 const express = require('express');
 const app = express();
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const proxy = httpProxy.createProxyServer({});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 const port = 3000;
-app.use('/kumes', createProxyMiddleware({ 
-    target: 'http://192.168.1.30:80', 
-    changeOrigin: true,
-    pathRewrite: {
-        '^/kumes': '/', // /server ile başlayan yolları hedef sunucuda / olarak değiştirir
-    },
-}));
+app.use('/server', (req, res) => {
+    proxy.web(req, res, { target: 'http://192.168.1.30:80' });
+});
 
 app.use("/", serverRouter);
 

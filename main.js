8d32bfa21c914ccd55016eff9c6788d1d3b3c4af
applusinc <help.applusinc@gmail.com>
2024-07-01 10:50:23 +0300

    kumes update

--- main.js
+++ main.js
@@ -1,17 +1,24 @@
-const dotenv = require('dotenv');
-dotenv.config();
-//require('dotenv').config({ path: '/full/custom/path/to/your/env' })
+
 const serverRouter = require("./routes/server/server");
 const express = require('express');
 const app = express();
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 const port = 3000;
-
+app.use('/kumes', createProxyMiddleware({ 
+    target: 'http://192.168.1.30:80', 
+    changeOrigin: true,
+    pathRewrite: {
+        '^/kumes': '/', // /server ile başlayan yolları hedef sunucuda / olarak değiştirir
+    },
+}));
 
 app.use("/", serverRouter);
 
 
+
 app.listen(port, () => console.log(`Server listening on ${port}!`));
+

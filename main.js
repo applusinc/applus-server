c94bcc77466476907fe5d46784242646d41ee14e
applusinc <help.applusinc@gmail.com>
2024-07-06 12:50:41 +0300

    kumes auth

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-
 const serverRouter = require("./routes/server/server");
 const httpProxy = require('http-proxy');
 const express = require('express');
+const basicAuth = require('basic-auth');
 const app = express();
 const proxy = httpProxy.createProxyServer({});
 
@@ -9,19 +9,31 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 const port = 3000;
-app.use('/kumes', (req, res) => {
-   
-    proxy.web(req, res, { target: 'http://192.168.1.30:80' }, function(e) {res.status(500).end("port unavailable: Lütfen yönetici ile iletişime geçin.")});
-    
+
+// KUMES AUTH
+const auth = function (req, res, next) {
+    const user = basicAuth(req);
+    if (!user || user.name !== 'admin' || user.pass !== 'apolandin') {
+        res.set('WWW-Authenticate', 'Basic realm="example"');
+        return res.status(401).send('Authentication required.');
+    }
+    next();
+};
+
+//KUMES PROXY 192.168.1.30:80
+app.use('/kumes', auth, (req, res) => {
+    proxy.web(req, res, { target: 'http://192.168.1.30:80' }, function (e) {
+        res.status(500).end("port unavailable: Lutfen yonetici ile iletisime gecin.");
+    });
 });
 
+// SERVER PROXY 192.168.1.31:4000
 app.use('/', (req, res) => {
-    proxy.web(req, res, { target: 'http://192.168.1.31:4000' }, function(e) {res.status(500).end("port unavailable: Lütfen yönetici ile iletişime geçin.")});
+    proxy.web(req, res, { target: 'http://192.168.1.31:4000' }, function (e) {
+        res.status(500).end("port unavailable: Lutfen yonetici ile iletisime gecin.");
+    });
 });
 
 app.use("/", serverRouter);
 
-
-
 app.listen(port, () => console.log(`Server listening on ${port}!`));
-

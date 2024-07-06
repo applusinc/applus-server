07a7c813025e02103e2b9e4488961053aa4b55e5
applusinc <help.applusinc@gmail.com>
2024-07-06 12:26:20 +0300

    PNAEL TO HOME

--- routes/server/server.js
+++ routes/server/server.js
@@ -2,9 +2,7 @@ const express = require('express');
 
 const serverRouter = express.Router();
 
-serverRouter.get('/', (req, res) => {
-    res.status(200).end('Hello World!')
-});
+
 serverRouter.get('/ping', (req, res) => {
   res.status(200).end('ping');
 });
